e81d9a122caaad44928bbc0e6fdabbb2fd9f535a
Squidward <1zhal9991@gmail.com>
2021-08-26 13:28:59 +0700

    Commit pertama

---
+++ database/connection.php
@@ -0,0 +1,9 @@
+<?php
+
+$username = "root";
+$password = "";
+$host = "localhost";
+$dbname = "keputusan";
+
+
+$connect = mysqli_connect($host, $username, $password, $dbname);
---
+++ layout/footer.php
@@ -0,0 +1,44 @@
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+
+<!-- Main Footer -->
+<footer class="main-footer">
+    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> 3.0.3-pre
+    </div>
+</footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.js"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="dist/js/demo.js"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+<script src="assets/admin-lte/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="assets/admin-lte/plugins/raphael/raphael.min.js"></script>
+<script src="assets/admin-lte/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="assets/admin-lte/plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<!-- ChartJS -->
+<script src="assets/admin-lte/plugins/chart.js/Chart.min.js"></script>
+
+<!-- PAGE SCRIPTS -->
+<script src="assets/admin-lte/dist/js/pages/dashboard2.js"></script>
+</body>
+
+</html>
