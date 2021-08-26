2cbd990b17e5358b5cb967736889bca295d6fa72
iqbalatma <35129050+iqbalatma@users.noreply.github.com>
2021-08-26 14:00:12 +0700

    restrukturisasi folder

---
+++ progress/progress_login.php
@@ -0,0 +1,25 @@
+<?php
+
+require("../database/connection.php");
+$username_form = $_POST["username"];
+$password_form = $_POST["password"];
+
+if (isset($_POST)) {
+    $query = mysqli_query($connect, "SELECT * FROM users WHERE username = '$username_form' AND password = '$password_form'");
+
+    $data = mysqli_fetch_assoc($query);
+
+    if ($query->num_rows > 0) {
+        $data_session = [
+            "is_logged_in" => true,
+            "username" => $data["username"],
+        ];
+        session_start();
+        $_SESSION = $data_session;
+        //kalau password benar
+        header("Location: ../index.php");
+    } else {
+        //kalau password salah
+        header("Location: ../login.php");
+    }
+}
